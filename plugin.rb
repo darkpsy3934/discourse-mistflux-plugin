a95568217c478fe1e5e7c89d31e2a6f8756a62a2
Jessica Cappella <4506061+darkpsy3934@users.noreply.github.com>
2024-08-29 15:06:00 +0100

    Resolve error on rake compile

--- plugin.rb
+++ plugin.rb
@@ -3,6 +3,3 @@
 # version: 1.0
 # authors: Jessica C.
 # url: https://github.com/darkpsy3934/discourse-mistflux-plugin
-
-register_asset "javascripts/discourse/components/m-pinned-topics-body.js", :server_side
-register_asset "javascripts/discourse/templates/connectors/before-topic-list-body/pinned-topics-tbody.js", :server_side
