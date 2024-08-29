d34bd320e3c39b93cecc66bf3617665e0fd2f8d6
Jessica Cappella <4506061+darkpsy3934@users.noreply.github.com>
2024-08-29 16:42:51 +0100

    Added hbs files for template rendering

--- assets/javascripts/discourse/components/m-pinned-topics-body.js
+++ assets/javascripts/discourse/components/m-pinned-topics-body.js
@@ -1,28 +1,8 @@
 import Component from "@ember/component";
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { createTemplateFactory } from "@ember/template-factory";
-
-const __COLOCATED_TEMPLATE__ = createTemplateFactory(
-    /*
-        {{#if this.pinnedTopics}}
-        <tbody class="pinned-topics">
-            {{#each this.pinnedTopics as |topic|}}
-                {{topic-list-item
-                  topic=topic
-                  bulkSelectEnabled=@bulkSelectEnabled
-                  lastVisitedTopic=@lastVisitedTopic
-                  selected=@selected
-                  hideCategory=@hideCategory
-                }}
-            {{/each}}
-            <tr class="table-break">
-                <td colspan="10">&nbsp;</td>
-            </tr>
-        </tbody>
-        {{/if}}
-     */
-);
+import { setComponentTemplate } from "@ember/component";
+import layout from '../templates/components/m-pinned-topics-body.hbs'; // Import the template
 
 export default class MPinnedTopicsBody extends Component {
     @tracked topics;
@@ -32,4 +12,4 @@ export default class MPinnedTopicsBody extends Component {
     }
 }
 
-setComponentTemplate(__COLOCATED_TEMPLATE__, MPinnedTopicsBody);
+setComponentTemplate(layout, MPinnedTopicsBody);
