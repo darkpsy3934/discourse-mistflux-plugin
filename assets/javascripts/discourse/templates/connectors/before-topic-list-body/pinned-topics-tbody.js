fb048ef1f4a9a4c9ed95852a4285b06abc7c4f7b
Jessica Cappella <4506061+darkpsy3934@users.noreply.github.com>
2024-09-01 21:11:13 +0100

    Update tag to apply the correct CSS styling

--- assets/javascripts/discourse/templates/connectors/before-topic-list-body/pinned-topics-tbody.js
+++ assets/javascripts/discourse/templates/connectors/before-topic-list-body/pinned-topics-tbody.js
@@ -1,5 +1,7 @@
 import Component from "@ember/component";
+import { classNames, tagName } from "@ember-decorators/component";
 
+@tagName("")
 export default class PinnedTopicsBody extends Component{
     get pinnedTopics() {
         return this.outletArgs.topics.filter((t) => t.get("pinned"));
