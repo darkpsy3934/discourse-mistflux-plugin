29438f246ecc174ed3f6f075a4cad41e9c9574e4
Jessica Cappella <4506061+darkpsy3934@users.noreply.github.com>
2024-09-01 07:23:01 +0100

    Update again for Discourse 3.4.0

--- assets/javascripts/discourse/components/m-pinned-topics-body.js
+++ assets/javascripts/discourse/components/m-pinned-topics-body.js
@@ -1,14 +1,16 @@
 import Component from "@ember/component";
-import { tracked } from '@glimmer/tracking';
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 
 export default class MPinnedTopicsBody extends Component {
     @tracked topics = [];
 
     get pinnedTopics() {
-        if (!this.args.topics ||!Array.isArray(this.args.topics)) {
-            console.error("topics is undefined");
+        // Defensive check for undefined topics
+        if (!this.args.topics || !Array.isArray(this.args.topics)) {
+            console.error("Topics are undefined or not an array");
             return [];
         }
-        return this.args.topics.filter(t => t.get("pinned"));
+        return this.args.topics.filter((t) => t.get("pinned"));
     }
 }
